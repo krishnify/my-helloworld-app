84fd88f4466d52d3791964943a22395d091192a1
krishnify <krishnify@gmail.com>
2025-11-15 05:10:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,17 @@
 import streamlit as st
-import graphviz
-
+import pandas as pd
+import numpy as np
+import time
 
 st.title('My New Hello World App')
-
 st.write('Hello world!')
 
-# Create a graphlib graph object
-graph = graphviz.Digraph()
-graph.edge("run", "intr")
-graph.edge("intr", "runbl")
-graph.edge("runbl", "run")
-graph.edge("run", "kernel")
-graph.edge("kernel", "zombie")
-graph.edge("kernel", "sleep")
-graph.edge("kernel", "runmem")
-graph.edge("sleep", "swap")
-graph.edge("swap", "runswap")
-graph.edge("runswap", "new")
-graph.edge("runswap", "runmem")
-graph.edge("new", "runmem")
-graph.edge("sleep", "runmem")
+df = pd.DataFrame(np.random.randn(15, 3), columns=(["A", "B", "C"]))
+my_data_element = st.line_chart(df)
 
-st.graphviz_chart(graph)
+for tick in range(10):
+    time.sleep(.5)
+    add_df = pd.DataFrame(np.random.randn(1, 3), columns=(["A", "B", "C"]))
+    my_data_element.add_rows(add_df)
+
+st.button("Regenerate")
