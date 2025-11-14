1bfac0f293d6a97e6c57c5819b8e19f9a9b3096e
krishnify <krishnify@gmail.com>
2025-11-14 07:45:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,30 @@ import streamlit as st
 st.title('My New Hello World App')
 
 st.write('Hello world!')
+
+import pandas as pd
+import streamlit as st
+
+data_df = pd.DataFrame(
+    {
+        "sales": [
+            [0, 4, 26, 80, 100, 40],
+            [80, 20, 80, 35, 40, 100],
+            [10, 20, 80, 80, 70, 0],
+            [10, 100, 20, 100, 30, 100],
+        ],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "sales": st.column_config.BarChartColumn(
+            "Sales (last 6 months)",
+            help="The sales volume in the last 6 months",
+            y_min=0,
+            y_max=100,
+        ),
+    },
+    hide_index=True,
+)
