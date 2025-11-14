e784b1ce665b5b2998baae8894d845fa9ab6e784
krishnify <krishnify@gmail.com>
2025-11-15 05:11:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ df = pd.DataFrame(np.random.randn(15, 3), columns=(["A", "B", "C"]))
 my_data_element = st.line_chart(df)
 
 for tick in range(10):
-    time.sleep(.5)
+    time.sleep(1)
     add_df = pd.DataFrame(np.random.randn(1, 3), columns=(["A", "B", "C"]))
     my_data_element.add_rows(add_df)
 
