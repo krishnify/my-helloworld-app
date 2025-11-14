cfa7d6485cd42514e74eb1a2a939391e092e19c0
krishnify <krishnify@gmail.com>
2025-11-14 19:54:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,25 @@
 import streamlit as st
+import graphviz
+
 
 st.title('My New Hello World App')
 
 st.write('Hello world!')
 
-import pandas as pd
-import streamlit as st
+# Create a graphlib graph object
+graph = graphviz.Digraph()
+graph.edge("run", "intr")
+graph.edge("intr", "runbl")
+graph.edge("runbl", "run")
+graph.edge("run", "kernel")
+graph.edge("kernel", "zombie")
+graph.edge("kernel", "sleep")
+graph.edge("kernel", "runmem")
+graph.edge("sleep", "swap")
+graph.edge("swap", "runswap")
+graph.edge("runswap", "new")
+graph.edge("runswap", "runmem")
+graph.edge("new", "runmem")
+graph.edge("sleep", "runmem")
 
-data_df = pd.DataFrame(
-    {
-        "sales": [
-            [0, 4, 26, 80, 100, 40],
-            [80, 20, 80, 35, 40, 100],
-            [10, 20, 80, 80, 70, 0],
-            [10, 100, 20, 100, 30, 100],
-        ],
-    }
-)
-st.dataframe(data_df)
-st.data_editor(
-    data_df,
-    column_config={
-        "sales": st.column_config.BarChartColumn(
-            "Sales (last 6 months)",
-            help="The sales volume in the last 6 months",
-            y_min=0,
-            y_max=100,
-        ),
-    },
-    hide_index=True,
-)
+st.graphviz_chart(graph)
