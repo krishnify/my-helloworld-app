73f043cfc395ba96448411280effd96b854ada2c
krishnify <krishnify@gmail.com>
2025-11-15 05:50:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ my_data_element = st.bar_chart(df)
 for tick in range(10):
     time.sleep(1)
     add_df = pd.DataFrame(np.random.randn(1, 3), columns=(["A", "B", "C"]))
+    my_data_element.delete_rows(df)
     my_data_element.add_rows(add_df)
 
 st.button("Regenerate")
